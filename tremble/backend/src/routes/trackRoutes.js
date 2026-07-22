fcf52f94d949625b056bedc88254294d2f5082aa
Mohit Mahajan <work.mahajanmohit@gmail.com>
2026-07-22 15:00:23 +0530

    Delete tremble directory

--- tremble/backend/src/routes/trackRoutes.js
+++
@@ -1,6 +0,0 @@
-import { Router } from "express";
-import { getTracks, searchTracks } from "../controllers/trackController.js";
-const router = Router();
-router.get("/", getTracks);
-router.get("/search", searchTracks);
-export default router;
